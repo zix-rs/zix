37ab4665fa503e21e97b00987f2f7922b2cf023c
arki <antoniorafaelariasromero@gmail.com>
2025-02-23 22:03:10 -0500

    refactor: updated import from ref_command to meta
    - Replaced `ref_command` imports with `meta` in `zx/src/app.rs`.
    - Removed unused `std::env` dependency and the `parse` function for cleaner code.

--- zx/src/app.rs
+++ zx/src/app.rs
@@ -7,37 +7,10 @@ use zix_core::entry::{
 use zix_core::entry::{Entry};
 
 use zix_utils::parser::parser;
-use crate::ref_command::{help, version, NAME, VERSION};
+use crate::meta::{help, version, NAME, VERSION};
 
 #[cfg(windows)]
 use glob::glob;
-
-use std::env;
-
-
-pub fn parse() -> (Vec<String>, Vec<String>)  {
-    let args = env::args().skip(1);
-
-    let mut options: Vec<String> = Vec::new();
-    let mut values: Vec<String> = Vec::new();
-
-    for arg in args {
-        if arg.starts_with("--") {
-            options.push(arg);
-        } else if arg.starts_with('-') && arg.len() > 1 {
-            options.extend(arg[1..].chars().map(|ch| format!("-{}", ch)));
-        } else {
-            values.push(arg);
-        }
-    }
-
-    if values.is_empty() {
-        values.push(".".to_string());
-    }
-
-    (options, values)
-}
-
 
 #[derive(Clone, Debug)]
 pub struct App {
