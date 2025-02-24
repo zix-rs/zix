4b8df0950b2c3c4e85bc68b4c6dea63be3e898b9
arki <antoniorafaelariasromero@gmail.com>
2025-02-23 22:12:19 -0500

    bin(core): for debug

---
+++ zix-core/src/bin/grid.rs
@@ -0,0 +1,36 @@
+use zix_core::grid;
+use colored::Colorize;
+
+fn main()   {
+    let output = grid::out::<String>(
+        vec![
+            " gagadfas".red().to_string(),
+            "gaga".red().to_string(),
+            "gaga".red().to_string(),
+            "gaga".red().to_string(),
+            " gaga".red().to_string(),
+            "gaga".red().to_string(),
+            " gaga".red().to_string(),
+            "gagadfsa".red().to_string(),
+            " gaga".red().to_string(),
+            "gaga".red().to_string(),
+            " gaga".red().to_string(),
+            "gaga".red().to_string(),
+            "gaga".red().to_string(),
+            "gdsafas".red().to_string(),
+            "gaga".red().to_string(),
+            "gads".red().to_string(),
+            "gaga".red().to_string(),
+            "gafdsafdsaga".red().to_string(),
+            "gaga".red().to_string(),
+            "gaga".red().to_string(),
+            "gaga".red().to_string(),
+            "gfdsafdsaaga".red().to_string(),
+            "gogo".blue().to_string(),
+            "gugu".blue().to_string()
+        ]
+    );
+
+    println!("{}", output)
+
+}
